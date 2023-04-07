f04cb2b1fbd947238bd489ef70a725fa646be5f8
TarakaKoda <taraka.koda@gmail.com>
2023-04-07 15:53:08 +0530

    decorators logic for basic login page

---
+++ login page example.py
@@ -0,0 +1,51 @@
+def outer_fun(fun):
+    def warpper_fun():
+        print("before")
+        val = fun()
+        print("after")
+        return val
+
+    return warpper_fun
+
+
+@outer_fun
+def message():
+    print("this is the main function")
+
+
+message()
+
+
+def login(fun):
+    def login_details(name, islogin):
+        if islogin == False:
+            print("please login first to continue further.")
+            return
+        # print("team uvw")
+        return fun(name, islogin)
+
+    return login_details
+
+
+@login
+def home_page(name, islogin):
+    print(f"welcome to the home page {name}")
+
+
+home_page("srinu", False)
+# my_homepage = login(home_page)
+# my_homepage("srinu", True)
+
+@login
+def about(name,islogin):
+    print(f"this is our about page {name}")
+
+
+about("pavan", True)
+
+
+def help_page():
+    print("this is your help page")
+
+
+help_page()
