0b08de1eb67f8873895bff167d28405355f952cb
TarakaKoda <taraka.koda@gmail.com>
2023-04-08 23:35:02 +0530

    basic example of class decorator

---
+++ class decorators.py
@@ -0,0 +1,26 @@
+class decorator_class(object):
+    def __init__(self, original_function):
+        self.original_function = original_function
+
+    def __call__(self, *args, **kwargs):
+        print(f"call method executes this before {self.original_function.__name__}")
+        return self.original_function(*args, **kwargs)
+
+    # we can add more functionality even after execution of our function.
+    # def __call__(self, *args, **kwargs):
+    #     print(f"call method executes this before {self.original_function.__name__}")
+    #     self.original_function(*args, **kwargs)
+    #     print("this is after original function execution")
+    #     return self.original_function(*args, **kwargs)
+
+@decorator_class
+def display(name,age):
+    print(f"name: {name}, age: {age}")
+
+@decorator_class
+def display_about():
+    print("this is our about page")
+
+
+display("srinu",22)
+display_about()
