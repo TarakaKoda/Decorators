3e1210ec570247637e83898b932a9c9a6cec0999
TarakaKoda <taraka.koda@gmail.com>
2023-04-09 00:10:17 +0530

    practical examples logging, time, functools(wraps) modules for better understanding

--- practical examples.py
+++ practical examples.py
@@ -1,35 +1,40 @@
 import time
+from functools import wraps
 
 
 def my_logger(original_function):
     import logging
     logging.basicConfig(filename=f"{original_function.__name__}", level=logging.INFO)
 
+    @wraps(original_function)
     def wrapper(*args, **kwargs):
         logging.info(
             f"Ran with args: {args}, and kwargs: {kwargs}"
         )
         return original_function(*args, **kwargs)
+
     return wrapper
 
 
 def my_timer(original_function):
     import time
+    @wraps(original_function)
     def wrapper(*args, **kwargs):
         t1 = time.time()
         result = original_function(*args, **kwargs)
         t2 = time.time() - t1
-        print(f"{original_function.__name__}  ran in: {t2}")
+        print(f"{original_function.__name__} ran in: {t2}")
         return result
+
     return wrapper
 
 
-
+@my_logger
 @my_timer
 def display_user_info(name, age):
     time.sleep(1)
     print(f"username: {name}, age: {age}")
 
 
-display_user_info("srinu", 22)
-display_user_info("pavan", 23)
+display_user_info("pavan", 22)
+
